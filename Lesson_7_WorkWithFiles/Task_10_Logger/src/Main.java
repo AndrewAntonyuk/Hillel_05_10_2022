d17059b37df6585bb4c6cc247436c6ac201a13d0
AndrewAntonyuk <antonyuk.andrij@gmail.com>
2022-10-30 13:06:09 +0200

    Remove tests folder

--- Lesson_7_WorkWithFiles/Task_10_Logger/src/Main.java
+++ Lesson_7_WorkWithFiles/Task_10_Logger/src/Main.java
@@ -1,21 +1,32 @@
 import hw_10_LoggerToFile.FileLogger;
 import hw_10_LoggerToFile.FileLoggerConfiguration;
+import hw_10_LoggerToFile.FileMaxSizeReachedException;
 import hw_10_LoggerToFile.LoggingLevel;
-import hw_10_Tests.TestLoggerClass1;
-import hw_10_Tests.TestLoggerClass2;
-
 public class Main {
     public static void main(String[] args) {
         FileLoggerConfiguration loggerConfiguration = new FileLoggerConfiguration("TestLogs.txt",
                 LoggingLevel.INFO, 1200);
         FileLogger logger = new FileLogger(loggerConfiguration);
-        TestLoggerClass1 loggerClass1 = new TestLoggerClass1(logger);
-        TestLoggerClass2 loggerClass2 = new TestLoggerClass2(logger);
+        logger.setInstanceName("TestFileLogger");
 
-        loggerClass1.testFoo();
+        try {
+            logger.debug("Test debug text 1");
+            logger.debug("Test debug text 2");
+            logger.debug("Test debug text 3");
+            logger.debug("Test debug text 4");
+            logger.info("Test info text 1");
 
-        loggerConfiguration.setCurrentLoggingLevel(LoggingLevel.DEBUG);
-        logger.setLoggerConfiguration(loggerConfiguration);
-        loggerClass2.testFoo();
+            loggerConfiguration.setCurrentLoggingLevel(LoggingLevel.DEBUG);
+            logger.setLoggerConfiguration(loggerConfiguration);
+
+            logger.debug("Test debug text 1");
+            logger.debug("Test debug text 2");
+            logger.debug("Test debug text 3");
+            logger.debug("Test debug text 4");
+            logger.info("Test info text 1");
+
+        } catch (FileMaxSizeReachedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- Lesson_7_WorkWithFiles/Task_10_Logger/src/hw_10_LoggerToFile/FileMaxSizeReachedException.java
+++ Lesson_7_WorkWithFiles/Task_10_Logger/src/hw_10_LoggerToFile/FileMaxSizeReachedException.java
@@ -1,14 +1,7 @@
 package hw_10_LoggerToFile;
 
 public class FileMaxSizeReachedException extends Exception{
-    public FileMaxSizeReachedException() {
-    }
-
     public FileMaxSizeReachedException(String message) {
         super(message);
-    }
-
-    public FileMaxSizeReachedException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
