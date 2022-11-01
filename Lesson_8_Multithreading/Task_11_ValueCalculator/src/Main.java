cfa1b5ac496f70822ec565468aa1e230be3f3998
AndrewAntonyuk <antonyuk.andrij@gmail.com>
2022-11-01 09:42:34 +0200

    Add Task 11: ValueCalculator Class

---
+++ Lesson_8_Multithreading/Task_11_ValueCalculator/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ValueCalculator calculator = new ValueCalculator();
+
+        calculator.doCalc();
+    }
+}
---
+++ Lesson_8_Multithreading/Task_11_ValueCalculator/src/ValueCalculator.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class ValueCalculator {
+    private final int size = 2000000;
+    private final int halfSize = size / 2;
+    private Float[] baseArray = new Float[size];
+
+    public void doCalc() {
+        long start = System.currentTimeMillis();
+        Function<Float[], Runnable> startThreadArray;
+        Float[] halfArray1 = new Float[halfSize];
+        Float[] halfArray2 = new Float[halfSize];
+        Thread threadHalfArray1;
+        Thread threadHalfArray2;
+
+        startThreadArray = (inputArray) -> {
+            return () -> {
+                Arrays.setAll(inputArray, (i) -> inputArray[i] = (float) (inputArray[i]
+                        * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5)
+                        * Math.cos(0.4f + (float) i / 2)));
+            };
+        };
+
+        Arrays.setAll(baseArray, n -> baseArray[n] = 3.25f);
+        System.arraycopy(baseArray, 0, halfArray1, 0, halfSize);
+        System.arraycopy(baseArray, halfSize, halfArray2, 0, halfSize);
+
+        threadHalfArray1 = new Thread(startThreadArray.apply(halfArray1));
+        threadHalfArray2 = new Thread(startThreadArray.apply(halfArray2));
+
+        threadHalfArray1.start();
+        threadHalfArray2.start();
+
+        try {
+            threadHalfArray1.join();
+            threadHalfArray2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.arraycopy(halfArray1, 0, baseArray, 0, halfSize);
+        System.arraycopy(halfArray2, 0, baseArray, halfSize, halfSize);
+
+        System.out.println("Running time is: " + (System.currentTimeMillis() - start) + " ms");
+    }
+}
