7cda784f4d56dfc52a0bf104ca113a20555f9a4a
Andrew Antonyuk <antonyuk.andrij@gmail.com>
2022-11-17 19:11:13 +0200

    Lesson 11 CoffeeOrderBoard: replace object reference to interface reference

--- Lesson_11_Queues/Task_15_CoffeeOrderBoard/src/coffee/order/CoffeeOrderBoard.java
+++ Lesson_11_Queues/Task_15_CoffeeOrderBoard/src/coffee/order/CoffeeOrderBoard.java
@@ -1,26 +1,27 @@
 package coffee.order;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class CoffeeOrderBoard {
-    private final LinkedList<Order> internalLinkedList = new LinkedList<Order>();
+    private final Queue<Order> internalQueue = new LinkedList<Order>();
     private Integer currentNumber = 1;
 
     public void add(String name) {
-        internalLinkedList.offer(new Order((currentNumber++), name));
+        internalQueue.offer(new Order((currentNumber++), name));
     }
 
     public void deliver() {
-        internalLinkedList.poll();
+        internalQueue.poll();
     }
 
     public void deliver(int number) {
-        internalLinkedList.removeIf(o -> o.getNumber() == number);
+        internalQueue.removeIf(o -> o.getNumber() == number);
     }
 
     public void draw() {
         System.out.println("Num  | Name");
         System.out.println("-----------");
-        internalLinkedList.forEach(o -> System.out.print(o.toString() + "\n"));
+        internalQueue.forEach(o -> System.out.print(o.toString() + "\n"));
     }
 }
